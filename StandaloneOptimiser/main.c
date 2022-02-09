78d918fbef19b78559b5f04586da9be4f7f385a3
Shadofisher <graemefisheratwork@gmail.com>
2022-02-09 06:09:00 +0200

    Bug fixes of paths that overflow memory

--- StandaloneOptimiser/main.c
+++ StandaloneOptimiser/main.c
@@ -1,4 +1,3 @@
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -12,7 +11,7 @@ static FILE * fp,*dp;
 typedef struct
 {
 
-	char line[20000][64];
+	char line[20000][128];
 	uint32_t len;
 	uint32_t valid;
 	uint32_t index;
@@ -37,6 +36,12 @@ void print_data(uint32_t list_len,gcode_t * gc)
         }
     }
 }
+
+uint32_t rangeLow;
+uint32_t rangeHigh;
+
+char * outfile;
+
 
 void calc_distance(uint32_t list_len,gcode_t * gc)
 {
@@ -68,7 +73,7 @@ void calc_distance(uint32_t list_len,gcode_t * gc)
         for (m = 0; m < list_len; m++)
         {
             //if ((gc[m].valid == 0) && (gc[m].len > 50))
-            if ((gc[m].valid == 0) && ((gc[m].len > 2) && (gc[m].len <= 50)))
+            if ((gc[m].valid == 0) && ((gc[m].len > rangeLow) && (gc[m].len <= rangeHigh)))
             {
                 complete = false;
                 sscanf(gc[m].line[0],"%s %c%f %c%f;",tmp,&c,&x2,&d,&y2);  // first comparison
@@ -100,13 +105,13 @@ void calc_distance(uint32_t list_len,gcode_t * gc)
                     closest_index = m;
                 }
 
-                printf("\n\rOld Distance: %f %d",old_distance,m);
+                //printf("\n\rOld Distance: %f %d",old_distance,m);
             }//else
             //printf("\n\rInvalid");
 
         }
 
-        printf("\n\rClosest index = %d %d %f",closest_index,direction,old_distance);
+        //printf("\n\rClosest index = %d %d %f",closest_index,direction,old_distance);
         gc[closest_index].valid = 1;
         index_count++;
         gc[closest_index].index = index_count;
@@ -119,37 +124,38 @@ void calc_distance(uint32_t list_len,gcode_t * gc)
         {
             sscanf(gc[closest_index].line[0],"%s %c%f %c%f;",tmp,&c,&x1,&d,&y1);
         }
-        printf("\n\r new X,Y %f %f %d",x1,y1,m);
+        //printf("\n\r new X,Y %f %f %d",x1,y1,m);
 
     }
 
     printf("\n\rDone %d",index_count);
 
-    if((dp = fopen("test.nc","w+")) == NULL)
+    if((dp = fopen(outfile,"w+")) == NULL)
     {
         perror("fopen");
         exit(1);
     }else
     {
-        printf("Writing gcode file.....");
+        printf("Writing gcode file.....(list_len: %d)",list_len);
 
         for (n = 0; n < list_len;n++)
         {
             for (m = 0; m < list_len; m++)
             {
-                if (gc[m].index == n)
+                if ((gc[m].index == n))
                 {
-                    fputs("\n\rG1 Z5\n\r",dp);
+                    fputs("G1 Z5\n",dp);
                     for (k = 0; k < gc[m].len; k++)
                     {
                         if (k == 1)
                         {
-                            fputs("\n\rG1 Z-2\n\r",dp);
+                            fputs("G1 Z-2\n",dp);
                         }
                         fputs(gc[m].line[k],dp);
+                        //fputs("\n",dp);
                     }
-                    sprintf(dist,";Distance: %f\n\r",gc[m].distance);
-                    fputs(dist,dp);
+                    //sprintf(dist,";Distance: %f\n\r",gc[m].distance);
+                    //fputs(dist,dp);
                     //fputs("\n\r;;;;;;;\n\r",dp);
 
                     break;
@@ -163,7 +169,7 @@ void calc_distance(uint32_t list_len,gcode_t * gc)
 
 }
 
-int main (void)
+int main (int argc, char *argv[])
 {
 	static uint32_t line_count = 0;
 	static uint32_t line_start = 0;
@@ -173,12 +179,38 @@ int main (void)
 	//static gcode_t gc_lines[10000];
 	gcode_t * gc_lines;
 
+	uint8_t start_new_list;
+
+	char * filename;
+
+	filename = argv[1];
+
+	rangeLow = atoi(argv[2]);
+	rangeHigh = atoi(argv[3]);
+    outfile = argv[4];
+	printf("Filename  = %s %d %d %s", argv[1],rangeLow,rangeHigh,outfile);
+    //exit(1);
 	gc_lines = malloc(10000 * sizeof(gcode_t));
 
 	float a;
 	a = sqrt(8);
 	printf("Gcode optimizer %f",a);
-	if ((fp=fopen("drawing.gcode","r")) == NULL)
+	start_new_list = 0;
+	#if 0
+    if ((fp=fopen(filename,"a")) == NULL)
+	{
+		perror("fopen:");
+		exit(1);
+	}else
+	{
+        fputs("G1 Z5;",fp);
+        fputs("\nG1 Z0;",fp);
+        fputs("\nG1 Z5;",fp);
+        fclose(fp);
+    }
+    #endif
+    //exit(1);
+	if ((fp=fopen(filename,"r")) == NULL)
 	{
 		perror("fopen:");
 		exit(1);
@@ -188,7 +220,7 @@ int main (void)
 		while (fgets(tmpstr,128,fp) != NULL)
 		{
 			//printf(tmpstr);
-			if (strstr(tmpstr,"G1 Z5") != NULL)
+			if ((strstr(tmpstr,"G1 Z5") != NULL) || (start_new_list))
 			{
 				if (line_start == 0)
 				{
@@ -202,6 +234,7 @@ int main (void)
 					//printf("\n\rLen: %d",gc_lines[list_count].len);
 					line_count = 0;
 					list_count++;
+					start_new_list = 0;
 				}
 			}else
 			{
@@ -211,6 +244,11 @@ int main (void)
                     {
                         strcpy(gc_lines[list_count].line[line_count],tmpstr);
                         line_count++;
+                        if (line_count >= 15000)
+                        {
+                            printf("---------------ERROR-------------------\n");
+                            start_new_list = 1;
+                        }
                     }
 				}
 			}
@@ -226,6 +264,13 @@ int main (void)
 			printf("\n\r %d %d %s ",n,gc_lines[n].len,gc_lines[n].line[m]);
 	}
 #endif
+
+//    for (m = 0; m < list_count; m++)
+//    {
+//       gc_lines[m].index =  list_count-1;      // initialise all
+//       gc_lines[m].valid =  0;      // initialise all
+
+//    }
 	calc_distance(list_count,gc_lines);
 	//print_data(list_count,gc_lines);
 	return (0);
